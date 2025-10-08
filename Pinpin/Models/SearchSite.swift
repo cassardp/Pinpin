77a198ddb0d636abde3a2037a824829afc35d507
Patrice Cassard <cassard2@gmail.com>
2025-10-08 10:09:55 +0200

    feat: configure app sandbox and remove Vinted from search sites

--- Pinpin/Models/SearchSite.swift
+++ Pinpin/Models/SearchSite.swift
@@ -26,7 +26,6 @@ struct SearchSite: Identifiable, Codable {
         SearchSite(name: "All", query: nil, iconName: "globe"),
         SearchSite(name: "Amazon", query: "amazon", iconName: "shippingbox.fill"),
         SearchSite(name: "Leboncoin", query: "leboncoin.fr", iconName: "tag"),
-        SearchSite(name: "Vinted", query: "vinted.com", iconName: "tshirt"),
         SearchSite(name: "eBay", query: "ebay.com", iconName: "chair"),
         SearchSite(name: "Etsy", query: "etsy", iconName: "paintpalette"),
         SearchSite(name: "Selency", query: "selency.fr", iconName: "lamp.desk"),
